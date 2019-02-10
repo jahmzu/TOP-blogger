1ad9c2b13a7fdbddbde2de1f0a18a12c2229d9cb
James Lee <jtlee86@gmail.com>
2019-02-10 02:15:09 +0000

    added ability to delete tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,8 +4,14 @@ class TagsController < ApplicationController
   end
   
   def index
-    
     @tags = Tag.all
+  end
+  
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+  
+    redirect_to tags_path
   end
   
 end
